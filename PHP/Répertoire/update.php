cfc141d00971b488a6b86ba2f713be9091950924
EmyFoxB <ebuidin@gmail.com>
2024-06-13 16:27:02 +0200

    bdd crud

---
+++ PHP/Répertoire/update.php
@@ -0,0 +1,43 @@
+<?php
+
+ob_start();
+require "auth.php";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="form-container">
+    <form method="POST">
+        <label for="nom">Nom:</label>
+        <input type="text" name="nom" required><br>
+
+        <label for="prenom">Prénom:</label>
+        <input type="text" name="prenom" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+
+        <label for="telephone">Téléphone:</label>
+        <input type="text" name="telephone" required><br>
+
+        <label for="role">Rôle:</label>
+
+        <select name="role" required>
+            <option value="admin">Admin</option>
+            <option value="non-admin">Non-Admin</option>
+        </select><br>
+
+        <input type="submit" value="Ajouter">
+    </form>
+</div>
+</body>
+</html>
